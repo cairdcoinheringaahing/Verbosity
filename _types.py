0cf39fc0a762e51eaba30d21ce4c550ec3664771
cairdcoinheringaahing <valyrio.email@gmail.com>
2017-10-05 21:26:36 +0100

    Update _types.py

--- _types.py
+++ _types.py
@@ -1,1 +1,14 @@
+import exceptions
 
+class BaseClass(object):
+	def __init__(self, value=None):
+		base = self.__class__.__mro__[1]
+		try: self.value = base(value)
+		except: self.value = base()
+			
+	def __run_method__(self, method, args):
+		try: getattr(self, method)
+		except: exceptions.RaiseException('UnknownMethodCall')
+		value = getattr(self, method)(*args)
+		self = value
+		return value

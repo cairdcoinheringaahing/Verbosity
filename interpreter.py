01fc0f61c99178c521bfdd05a901ba4b53e4b090
cairdcoinheringaahing <valyrio.email@gmail.com>
2017-10-05 23:39:13 +0100

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -68,29 +68,30 @@ def interpreter(code, stdin, ARGV, stdout):
 	code = parser.parser(code)
 	for line in code:
 		func = line[0]
+		
 		if func == 'Include':
 			new_type = line[1]
-			if len(new_type) > 1:
-				for new in new_type:
-					INCLUDES.append(PARENTS[new])
-			else:
-				INCLUDES.append(new_type[0])
+			INCLUDES.append(new_type[0])
+			
 		if re.search(r'({}):DefineVariable'.format('|'.join(INCLUDES)), func):
 			variable_type = PARENTS[func.split(':DefineVariable')[0]]
 			name = line[1][0]
 			value = line[1][1]
 			VARIABLES[name] = variable_type(value)
+			
 		elif re.search(r'({}):RedefineVariable'.format('|'.join(INCLUDES)), func):
 			name = line[1][0]
 			new_value = line[1][1]
 			try: variable_type = VARIABLES[name].type_of
 			except: raise AttemptToRedefineUnknownVariable(name)
 			VARIABLES[name] = variable_type(value)
+			
 		elif func.split(':')[0] in INCLUDES:
 			type_ = PARENTS[func.split(':')[0]]
 			method = func.split(':')[1]
 			args = list(args_eval(line[1]))
 			type_.__run_method__(method, args)
+			
 		elif func.split(':')[0] in VARIABLES.keys():
 			var = VARIABLES[func.split(':')[0]]
 			method = func.split(':')[1]

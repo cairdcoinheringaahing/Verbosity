8d7c28bfd009beb987de66381328f8fce533a9dd
cairdcoinheringaahing <valyrio.email@gmail.com>
2017-10-07 20:05:23 +0100

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -4,6 +4,7 @@ import sys
 import exceptions
 import parser
 import _types as types
+import _structs as structures
 
 VARIABLES = {}
 INCLUDES = []
@@ -19,11 +20,11 @@ PARENTS = {'Array':types.Array,
            'String':types.String,
 	   'Set':types.Set}
 
-STRUCTURES = {'Class':types.Structures.Class,
-	      'Conditional':types.Structures.Conditional,
-	      'Error':types.Structures.Error,
-	      'Function':types.Structures.Function,
-	      'Loop':types.Structures.Loop}
+STRUCTURES = {'Class':structures.Class,
+	      'Conditional':structures.Conditional,
+	      'Error':structures.Error,
+	      'Function':structures.Function,
+	      'Loop':structures.Loop}
 
 def isint(string):
 	return re.search(r'^-?\d+$', string) and 'Integer' in INCLUDES
@@ -37,65 +38,72 @@ def iscomplex(string):
 def isstring(string):
 	return re.search(r'^"|(""").*\1$', string) and 'String' in INCLUDES
 
-def arg_eval(args):
-	for arg in args:
-		if isint(arg):
-			yield types.Integer(arg)
-		if isreal(arg):
-			yield types.FloatingPoint(arg)
-		if iscomplex(arg):
-			yield types.Complex(arg)
-		if isstring(arg):
-			yield types.String(arg)
-		if arg.split(':')[0] in INCLUDES:
-			type_ = PARENTS[arg.split(':')[0]]
-			method = arg.split(':')[1]
-			a = list(arg_eval(arg))
-			yield type_.__run_method__(method, a)
-		if arg.split(':')[0] in VARIABLES.keys():
-			var = VARIABLES[arg.split(':')[0]]
-			method = arg.aplit(':')[1]
-			a = list(arg_eval(arg))
-			yield var.__run_method__(method, a)
-			
-		try:
-			yield eval("types." + str(arg))
-		except:
-			try: yield eval(arg)
-			except: yield []
+def standard_eval(*args):
+	for value in args:
+		if isint(value):
+			yield types.Integer(value)
+		if isreal(value):
+			yield types.FloatingPoint(value)
+		if iscomplex(value):
+			yield types.Complex(value)
+		if isstring(value):
+			yield types.String(value)
+		if value in VARIABLES.keys():
+			yield VARIABLES[value]
 
-def interpreter(code, stdin, ARGV, stdout):
+def define_eval(*args):
+	args = args[:(args[0].split(':')[0] in INCLUDES)+1]
+	if len(args) == 1:
+		value = args[0]
+	else:
+		type_, method = args[0].split(':')
+		type_ = PARENTS[type_]
+		new_args = list(standard_eval(*args[1]))
+		return type_.__run_method__(type_, method, new_args)
+
+	if isint(value):
+		return types.Integer(value)
+	if isreal(value):
+		return types.FloatingPoint(value)
+	if iscomplex(value):
+		return types.Complex(value)
+	if isstring(value):
+		return types.String(value)
+
+def handle_line(line):
+	func = line[0]
+		
+	if func == 'Include':
+		new_type = line[1]
+		INCLUDES.append(new_type[0])
+			
+	elif re.search(r'({}):DefineVariable'.format('|'.join(INCLUDES)), func):
+		variable_type = PARENTS[func.split(':DefineVariable')[0]]
+		name = line[1][0]
+		value = define_eval(*line[1][1:])
+		VARIABLES[name] = variable_type(value)
+			
+	elif re.search(r'({}):RedefineVariable'.format('|'.join(INCLUDES)), func):
+		name = line[1][0]
+		new_value = define_eval(*line[1][1:])
+		try: variable_type = VARIABLES[name].__class__
+		except: exceptions.RaiseException('AttemptToRedefineUnknownVariable')
+		VARIABLES[name] = variable_type(new_value)
+			
+	elif func.split(':')[0] in INCLUDES:
+		type_ = PARENTS[func.split(':')[0]]
+		method = func.split(':')[1]
+		args = list(standard_eval(*line[1]))
+		type_.__run_method__(type_, method, args)
+			
+	elif func.split(':')[0] in VARIABLES.keys():
+		var = VARIABLES[func.split(':')[0]]
+		type_ = var.__class__
+		method = func.split(':')[1]
+		args = list(standard_eval(*line[1]))
+		type_.__run_method__(var, method, args)
+
+def interpreter(code):
 	code = parser.parser(code)
-	for line in code:
-		func = line[0]
-		
-		if func == 'Include':
-			new_type = line[1]
-			INCLUDES.append(new_type[0])
-			
-		if re.search(r'({}):DefineVariable'.format('|'.join(INCLUDES)), func):
-			variable_type = PARENTS[func.split(':DefineVariable')[0]]
-			name = line[1][0]
-			value = line[1][1]
-			VARIABLES[name] = variable_type(value)
-			
-		elif re.search(r'({}):RedefineVariable'.format('|'.join(INCLUDES)), func):
-			name = line[1][0]
-			new_value = line[1][1]
-			try: variable_type = VARIABLES[name].type_of
-			except: raise AttemptToRedefineUnknownVariable(name)
-			VARIABLES[name] = variable_type(value)
-			
-		elif func.split(':')[0] in INCLUDES:
-			type_ = PARENTS[func.split(':')[0]]
-			method = func.split(':')[1]
-			args = list(args_eval(line[1]))
-			type_.__run_method__(method, args)
-			
-		elif func.split(':')[0] in VARIABLES.keys():
-			var = VARIABLES[func.split(':')[0]]
-			method = func.split(':')[1]
-			args = list(args_eval(line[1]))
-			var.__run_method__(method, args)
-		
-print(INCLUDES)
+	for i, line in enumerate(code, 1):
+		handle_line(line)

bbd4e67327e323805a367f4bd05915b44d4da81f
cairdcoinheringaahing <valyrio.email@gmail.com>
2017-11-25 19:20:43 +0000

    Added _types.py

--- _types.py
+++ _types.py
@@ -1,66 +1,135 @@
-import exceptions
+import sys
+
+import _exceptions as exceptions
 
 class BaseClass(object):
-	def __init__(self, value=None):
-		self.base = self.__class__.__mro__[2]
-		try: self.value = self.base(value)
-		except: self.value = self.base()
-	
-	@staticmethod
-	def __run_method__(master, method, args):
-		try: func = getattr(master, method)
-		except: return exceptions.RaiseException('UnknownMethodCall')
-		value = func(master, *args)
-		master.value = value
-		return value
+    def __init__(self, value=None):
+        try: self.base = self.__class__.__mro__[2]
+        except: self.base = self.__class__
+        try: self.value = self.base(value)
+        except: self.value = self.base()
+        
+    @staticmethod
+    def __run_method__(master, method, args):
+        try: func = getattr(master, method)
+        except: return exceptions.RaiseException('UnknownMethodCall')
+        value = func(*args)
+        master.value = value
+        return value
 
-	def __repr__(self):
-		return '{}<{}>'.format(self.__class__.__name__, self.value)
+    def __repr__(self):
+        return '{}<{}>'.format(self.__class__.__name__, self.value)
 
 class Array(BaseClass, list):
-	def __repr__(self):
-		return "Array<{}>".format("; ".join(map(str, self.value)))
+    def __repr__(self):
+        return "Array<{}>".format("; ".join(map(str, self.value)))
 
 class Binary(BaseClass):
-	def __repr__(self):
-		return "BinaryInteger<{}>".format("".join(map(str, self.value)))
+    def __repr__(self):
+        return "BinaryInteger<{}>".format("".join(map(str, self.value)))
 
 class Boolean(BaseClass):
-	def __repr__(self):
-		return "Boolean<{}>".format(["False", "True"][self.value])
+    TRUE = True
+    FALSE = False
+
+    def __init__(self, value=None):
+        self.value = bool(value)
+    
+    def __repr__(self):
+        return "Boolean<{}>".format("TRUE" if self.value else "FALSE")
+
+    def LessThan(self, x, y):
+        return x.value < y.value
+
+    def GreaterThan(self, x, y):
+        return x.value > y.value
+
+    def ArgumentsAreEqual(self, x, y):
+        return x.value == y.value
+
+    def GreaterThanOrEqualTo(self, x, y):
+        return x.value >= y.value
+
+    def LessThanOrEqualTo(self, x, y):
+        return x.value <= y.value
+
+    def LogicalNot(self, value):
+        return not value.value
+
+    def LogicalAnd(self, x, y):
+        return x.value and y.value
+
+    def LogicalOr(self, x, y):
+        return x.value or y.value
 
 class Complex(BaseClass, complex):
-	def __init__(self, real, imag):
-		self.real = real
-		self.imag = imag
-		
-	def __repr__(self):
-		return "Complex<{}; {}>".format(self.real, self.imag)
+    def __init__(self, real, imag):
+        self.REAL = real
+        self.IMAGINARY = imag
+                
+    def __repr__(self):
+        return "Complex<{}; {}>".format(self.real, self.imag)
 
 class Dictionary(BaseClass, dict):
-	pass
+    pass
 
 class FloatingPoint(BaseClass, float):
-	pass
+    pass
 
 class Input(BaseClass):
-	def __repr__(self):
-		return "Input metaclass"
+    def __repr__(self):
+        return "Input metaclass"
 
 class Integer(BaseClass, int):
-	def Increment(self):
-		return self.value + 1
-	
-	def Decrement(self):
-		return self.value - 1
+    def Increment(self):
+        return self.value + 1
+        
+    def Decrement(self):
+        return self.value - 1
+
+    def Sum(self, y):
+        return self.value + y.value
+
+    def Difference(self, y):
+        return self.value - y.value
+
+    def Product(self, y):
+        return self.value * y.value
+
+    def Quotient(self, y):
+        if type(y) != Integer:
+            exceptions.RaiseException('Right argument must be Integer type')
+        return self.value / y.value
+
+    __str__ = BaseClass.__repr__
 
 class Output(BaseClass):
-	def __repr__(self):
-		return "Output metaclass"
+    FILE = sys.stdout
+    LAST = ''
+    WRITTEN = ''
+    
+    def __repr__(self):
+        return "Output metaclass"
+
+    def DisplayAsText(self, text, end='\n'):
+        text, end = map(str, [text, end])
+        Output.FILE.write(text + end)
+        Output.LAST = text
+        Output.WRITTEN += text + end
 
 class String(BaseClass, str):
-	pass
+    def TakeLastCharacters(self, n):
+        return self.value[Integer(-n):]
+    
+    def TakeFirstCharacters(self, n):
+        return self.value[:Integer(-n)]
 
-class Set(BaseClass, set):
-	def __repr__(self):
-		return "Set<{}>".format("; ".join(map(str, self.value)))
+    def RemoveCharactersFromStart(self, n):
+        return self.value[Integer(n):]
+
+    def RemoveCharactersFromEnd(self, n):
+        return self.value[:Integer(n)]
+
+class UnorderedSet(BaseClass, set):
+    def __repr__(self):
+        return "UnorderedSet<{}>".format("; ".join(map(str, self.value)))
